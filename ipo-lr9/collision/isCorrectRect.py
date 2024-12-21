6d0bbabbdcf86cc9fc1fb36020eba10905457eeb
vladosicks1 <goborovvlad@gmail.com>
2024-12-21 11:42:32 +0300

    Update isCorrectRect.py

--- ipo-lr9/collision/isCorrectRect.py
+++ ipo-lr9/collision/isCorrectRect.py
@@ -1,1 +1,10 @@
 
+class RectCorrectError(Exception):
+    pass
+
+def isCorrectRect(rectangles):
+    for i in rectangles:
+        if i[0][0] >= i[1][0] or i[0][1] >= i[1][1]:
+            raise RectCorrectError('Один из прямоугольников некорректный')
+
+    return True
